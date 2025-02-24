6b9a39a948fafb42d143010286023c4882750600
Ajay.tiwari <ajay.tiwari@cloudanalogy.com>
2025-02-24 20:08:28 +0530

    asdasd

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const fs = require('fs')
 const secrets = {
-  dbUri: dbUri: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin`,
+  dbUri: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin`,
   dbUriLocal: process.env.MONGO_DB_LOCAL
  };
 
