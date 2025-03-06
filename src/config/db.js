ca56a09c4de375fe861e8553c2bc64bb1b96a1aa
Ajay.tiwari <ajay.tiwari@cloudanalogy.com>
2025-03-06 16:11:14 +0530

    changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const fs = require('fs')
 const secrets = {
-  dbUri: `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin&replicaSet=${process.env.MONGO_RS}` || "YOUR MONGODB URI HERE",
+  dbUri: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin`,
   dbUriLocal: process.env.MONGO_DB_LOCAL
  };
 
